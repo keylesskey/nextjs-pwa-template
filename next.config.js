138466986d036e9c40f95e4a3e2cdc574c8eac09
keylesskey <160275913+keylesskey@users.noreply.github.com>
2024-03-21 20:12:37 +0100

    Updated

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const runtimeCaching = require('next-pwa/cache');
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // 👈 DISABLING PWA IN DEVELOPMENT MODE
+  // disable: process.env.NODE_ENV === 'development',// 👈 DISABLING PWA IN DEVELOPMENT MODE
   register: true,
   skipWaiting: true,
   runtimeCaching,
